bdffa4987beb6ff898408e5875126a4fb20339ec
Olaide <olaidemmanuel1@gmail.com>
2020-06-20 05:56:50 +0100

    Prepared the interface and made API was gotten

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+const questionApi = "https://opentdb.com/api.php?amount=40&category=9&difficulty=medium&type=multiple";
+
+function App() {
+    return (
+         <div className = 'App'>
+
+        <h1> Emmanuel </h1>
+        </div>
+    );
+}
+
+export default App;
