bcc0cda966ea0eab02776b6bb08b01a6c194190a
Olaide <olaidemmanuel1@gmail.com>
2020-06-21 19:46:42 +0100

    Still on the functionalities

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,11 @@ import './App.css';
 const questionApi = "https://opentdb.com/api.php?amount=40&category=9&difficulty=medium&type=multiple";
 
 const App = () =>{
-    const [questions, setQuestion] = useState([])
+    const [questions, setQuestion] = useState([]);
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const [ score, setScore] = useState(0);
+    const [showAnswer, setShowAnswer] = useState(false);
+
 
     useEffect(() => {
         fetch(questionApi)
@@ -16,20 +20,36 @@ const App = () =>{
     }, []);
 
     const handleAnswer = (answer) => {
+        const newIndex  = currentIndex + 1;
+        setCurrentIndex(newIndex);
+            // prevent double answers
+            // Increase the score
+            if(!showAnswer) {
+                    if(answer === questions[currentIndex].correct_answer) {
+                        setScore(score + 1);
+                    }
+                }
+    };
+      setShowAnswer(true)
 
-    }
-    return (
+    return  (
         questions.length > 0 ? (
             <div className ='container'>
-              <Questionaire data={questions[0]}
-                  handleAnswer={handleAnswer}
-              />
+                {currentIndex >= questions.length ? (
+                    <h1 className="text-3xl text-red font-bold"> Your score is {score} </h1>)  :
+                        <Questionaire
+                        data={questions[currentIndex]}
+                            handleAnswer={handleAnswer}
+                            showAnswer={showAnswer}
+                        />
+                }
             </div>
         ) :
         <h2 className="text-2xl text-white font-bold">
             (Loading question)
         </h2>
     );
+
 }
 
 export default App;
